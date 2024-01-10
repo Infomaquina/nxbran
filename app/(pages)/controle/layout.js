2bfeecdcd9a48fe8db83f0024cffb100255236e2
Brandev <brandeveloper@outlook.br>
2024-01-10 13:21:44 -0300

    🔧 REfactor

--- app/(pages)/controle/layout.js
+++ app/(pages)/controle/layout.js
@@ -1,10 +1,4 @@
-import { getCookies } from 'next-client-cookies/server';
-
 export default function layout({children, modal}) {
-
-   const cookies = getCookies();
-   console.log(cookies.get('ponto'));
-   
    return (<>
       {children}
       {modal}
