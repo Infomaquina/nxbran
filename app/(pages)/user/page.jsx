d382f0d509d86c4f6804fec39b7ebfef451cdf38
Brandev <brandeveloper@outlook.br>
2024-01-20 20:23:47 -0300

    🔀 Busca users

--- app/(pages)/user/page.jsx
+++ app/(pages)/user/page.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Image from "next/image"
+import UserSession from "@/app/components/UserSession"
+import FloatingLabel from "react-bootstrap/FloatingLabel"
+import Form from "react-bootstrap/Form"
+import { faUsers } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import executeQuery from "@/database/sql"
 
-export default function user() {
-  return (
-    <div>user</div>
-  )
+export default async function user() {
+   const session = await UserSession()
+   let users = await executeQuery("SELECT * FROM users")
+
+  return (<>
+   <div className="row">
+      <div className="col-4 text-center">
+         <Image src={session.user.image} className="rounded border border-2 shadow" alt="User Image" height={105} width={105} />
+      </div>
+      <div className="col-8">
+         <FloatingLabel controlId="floatingSelect" 
+            label={<><FontAwesomeIcon icon={faUsers}/> Usuários</>}>
+            <Form.Select>
+               {users.map((user) => (
+                  <option key={user.id} value={user.id}>{user.name}</option>
+               ))}
+            </Form.Select>
+         </FloatingLabel>
+      </div>
+  </div>
+  </>)
 }
