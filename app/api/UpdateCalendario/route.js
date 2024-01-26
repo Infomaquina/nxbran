892fc1ed3a019844c1297d86282104e09c27d5c9
Brandev <brandeveloper@outlook.br>
2024-01-26 09:43:59 -0300

    🔧 Relavidate obrigatorio

--- app/api/UpdateCalendario/route.js
+++ app/api/UpdateCalendario/route.js
@@ -1,10 +1,13 @@
 import executeQuery from '@/database/sql'
 import { NextResponse } from 'next/server'
+import { revalidatePath } from 'next/cache';
 
 export async function POST(req, res){
    const query = await req.json()
    
    await executeQuery("INSERT INTO folgas (data, id_user) VALUES (?,?)",[query.data,query.id_user])
+
+   revalidatePath('/user')
 
    return NextResponse.json('ok')
 }
@@ -14,6 +17,7 @@ export async function DELETE(req, res){
       const url = new URL(req.url)
       const data = url.searchParams.get('data')
       await executeQuery("DELETE FROM folgas WHERE data =?",[data])
+      revalidatePath('/user')
       return NextResponse.json('ok')
    } catch (error) {
       
