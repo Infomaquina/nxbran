e4e185ad63073c0ba491402b066557d2eb328372
Brandev <brandeveloper@outlook.br>
2024-02-15 16:37:05 -0300

    🔐 Tabela sem boostrap

--- app/(pages)/tabela/page.jsx
+++ app/(pages)/tabela/page.jsx
@@ -3,30 +3,30 @@ import executeQuery from '@/database/sql'
 export default async function Tabela(){
 
    const data = await executeQuery(
-      "SELECT DATE_FORMAT(r.data, '%d/%m/%y') AS dia, DATE_FORMAT(r.data, '%H:%i:%s') AS hora, CASE momento WHEN 0 THEN 'entrada' WHEN 1 THEN 'intervalo' WHEN 2 THEN 'retorno' WHEN 3 THEN 'saida' END AS result, u.name FROM registros r LEFT JOIN users u ON r.id_user = u.id ORDER BY r.id DESC"
+      "SELECT DATE_FORMAT(r.data, '%d/%m/%y') AS dia, DATE_FORMAT(r.data, '%H:%i:%s') AS hora, CASE r.momento WHEN 0 THEN 'entrada' WHEN 1 THEN 'intervalo' WHEN 2 THEN 'retorno' WHEN 3 THEN 'saida' END AS result, u.name, u.cor FROM registros r LEFT JOIN users u ON r.id_user = u.id ORDER BY r.id DESC"
    )
 
    return (
-      <div>
-         <table className='table table-striped'>
-               <thead>
-                  <tr>
-                     <th>Dia</th>
-                     <th>Hora</th>
-                     <th>User</th>
-                     <th>Momento</th>
+      <div class="table-responsive">
+         <table className='custom-table'>
+            <thead>
+               <tr>
+                  <th>Dia</th>
+                  <th>Hora</th>
+                  <th>User</th>
+                  <th>Momento</th>
+               </tr>
+            </thead>
+            <tbody>
+               {data.map(item => (
+                  <tr key={item.id} style={{backgroundColor:item.cor}}>
+                     <td>{item.dia}</td>
+                     <td>{item.hora}</td>
+                     <td>{item.name}</td>
+                     <td>{item.result}</td>
                   </tr>
-               </thead>
-               <tbody>
-                  {data.map(item => (
-                     <tr key={item.id}>
-                        <td>{item.dia}</td>
-                        <td>{item.hora}</td>
-                        <td>{item.name}</td>
-                        <td>{item.result}</td>
-                     </tr>
-                  ))}
-               </tbody>
+               ))}
+            </tbody>
          </table>
       </div>
    );
