9202a5bb69d9b67616b7fcfe9219c6c3441e41ef
Brandev <brandeveloper@outlook.br>
2024-03-02 21:25:04 -0300

    ✨ Tabela copleta

--- app/(pages)/tabela/page.jsx
+++ app/(pages)/tabela/page.jsx
@@ -7,24 +7,34 @@ export default async function Tabela(){
    const day = dia.toString().padStart(2, '0');
    const mes = (hoje.getMonth() + 1).toString().padStart(2, '0');
    const ano = hoje.getFullYear();
-   const time = ano+'-'+mes+'-'+day 
+   const time = ano+'-'+mes 
 
    const data = await executeQuery(
-      "SELECT DATE_FORMAT(r.data, '%H:%i:%s') AS hora, CASE r.momento WHEN 0 THEN 'entrada' WHEN 1 THEN 'intervalo' WHEN 2 THEN 'retorno' WHEN 3 THEN 'saida' END AS result, u.name, u.cor FROM registros r LEFT JOIN users u ON r.id_user = u.id WHERE r.data LIKE '"+time+"%' ORDER BY r.id DESC"
+      "SELECT DATE_FORMAT(r.data, '%d %H:%i:%s') AS hora, DATE_FORMAT(r.data, '%d/%m') AS dia, CASE r.momento WHEN 0 THEN 'entrada' WHEN 1 THEN 'intervalo' WHEN 2 THEN 'retorno' WHEN 3 THEN 'saida' END AS result, u.name, u.cor FROM registros r LEFT JOIN users u ON r.id_user = u.id ORDER BY r.id DESC"
    )
 
 
    return (
       <div className="table-responsive">
-         <h5 className='text-white'>Hoje {dia}</h5>
          <table className='custom-table'>
-            {data.map(item => (
-               <tr key={item.id} className='text-white' style={{backgroundColor:item.cor}}>
-                  <td>{item.hora}</td>
-                  <td>{item.name}</td>
-                  <td>{item.result}</td>
+            <thead>
+               <tr className=' bg-white'>
+                  <td>Dia</td>
+                  <td>Hora</td>
+                  <td>User</td>
+                  <td>Ação</td>
                </tr>
-            ))}
+            </thead>
+            <tbody>
+               {data.map(item => (
+                  <tr key={item.id} className='text-white'>
+                     <td>{item.dia}</td>
+                     <td>{item.hora}</td>
+                     <td><span className='rounded p-2' style={{backgroundColor:item.cor}}>{item.name}</span></td>
+                     <td>{item.result}</td>
+                  </tr>
+               ))}
+            </tbody>
          </table>
       </div>
    );
