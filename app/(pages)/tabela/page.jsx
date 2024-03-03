a68d231967fdf6637d3f339bc0dd6a7528a62299
Brandev <brandeveloper@outlook.br>
2024-03-02 21:25:52 -0300

    🐛 Bug dia

--- app/(pages)/tabela/page.jsx
+++ app/(pages)/tabela/page.jsx
@@ -10,7 +10,7 @@ export default async function Tabela(){
    const time = ano+'-'+mes 
 
    const data = await executeQuery(
-      "SELECT DATE_FORMAT(r.data, '%d %H:%i:%s') AS hora, DATE_FORMAT(r.data, '%d/%m') AS dia, CASE r.momento WHEN 0 THEN 'entrada' WHEN 1 THEN 'intervalo' WHEN 2 THEN 'retorno' WHEN 3 THEN 'saida' END AS result, u.name, u.cor FROM registros r LEFT JOIN users u ON r.id_user = u.id ORDER BY r.id DESC"
+      "SELECT DATE_FORMAT(r.data, '%H:%i:%s') AS hora, DATE_FORMAT(r.data, '%d/%m') AS dia, CASE r.momento WHEN 0 THEN 'entrada' WHEN 1 THEN 'intervalo' WHEN 2 THEN 'retorno' WHEN 3 THEN 'saida' END AS result, u.name, u.cor FROM registros r LEFT JOIN users u ON r.id_user = u.id ORDER BY r.id DESC"
    )
 
 
