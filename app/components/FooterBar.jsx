941228ecd2f5028b47c450bf025cf331f94666b4
Brandev <brandeveloper@outlook.br>
2024-03-02 21:47:49 -0300

    🔒️ Tabela Liberada

--- app/components/FooterBar.jsx
+++ app/components/FooterBar.jsx
@@ -4,10 +4,10 @@ import { faCalendarDays } from '@fortawesome/free-solid-svg-icons'
 import { faChartColumn } from '@fortawesome/free-solid-svg-icons'
 import Button from 'react-bootstrap/Button';
 import Link from 'next/link'
-import UserSession from '@/app/components/UserSession'; 
+// import UserSession from '@/app/components/UserSession'; 
 
 export default async function FooterBar() {
-   const session = await UserSession()
+   // const session = await UserSession()
    return (<>
       <nav className="navbar fixed-bottom bg-dark p-0">
          <div className="container-fluid">
@@ -21,13 +21,11 @@ export default async function FooterBar() {
                   <FontAwesomeIcon icon={faCalendarDays} className='fa-2x text-success'/>
                </Link>
             </Button>
-            {session.user.level == 0 &&(
-               <Button type='button' variant='dark' className='animate__animated animate__lightSpeedInLeft animate__faster'>
-                  <Link href={'/tabela'}>
-                     <FontAwesomeIcon icon={faChartColumn} className='fa-2x text-success'/>
-                  </Link>
-               </Button>
-            )}
+            <Button type='button' variant='dark' className='animate__animated animate__lightSpeedInLeft animate__faster'>
+               <Link href={'/tabela'}>
+                  <FontAwesomeIcon icon={faChartColumn} className='fa-2x text-success'/>
+               </Link>
+            </Button>
          </div>
       </nav>
    </>);
